92afaa9b79b304f50a691d1f9a20abbe27ab604f
mummy-alive <mummyee@ewhain.net>
2022-11-27 15:46:28 +0000

    week12 백엔드 팀 작업
    1. 맛집리스트 화면 페이징
    2. 맛집 리스트 화면, 맛집 세부 화면, 맛집 대표메뉴 화면, 맛집 리뷰 조회화면과 지난시간에 생성
    한 DB와 연동
    3. 화면 연결
     1) 맛집 리스트 화면 - 맛집 세부 화면
     2) 맛집 세부 화면 - 맛집 대표메뉴 조회하면
     3) 맛집 세부화면 - 맛집 리뷰 조회하면

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from database import DBhandler
 import sys
 
@@ -10,14 +10,17 @@ DB = DBhandler()
 def hello():
     return render_template("home.html")
 
-@application.route("/showRestaurantList")
-def view_list():
-    return render_template("/showRestaurantList.html")
-
-
 @application.route("/addRestaurant")
 def reg_restaurant():
     return render_template("addRestaurant.html")
+
+@application.route("/showRestaurantList")
+def view_list():
+    return render_template("showRestaurantList.html")
+
+@application.route("/showRestaurantDetail")
+def view_res_deatil():
+    return render_template("showRestaurantDetail.html")
 
 @application.route("/showRecommendation")
 def view_recomm_list():
@@ -33,40 +36,53 @@ def reg_bestmenu():
     print(list(data.values()))
     return render_template("addBestMenu.html", data=data)
 
-
 @application.route("/showBestMenu")
 def view_bestmenu():
     return render_template("showBestMenu.html")
 
-@application.route("/showBestMenu", methods=['POST'])
+@application.route("/addBestMenuResult")
+def reg_finish():
+    return render_template("addBestMenuResult.html")
+
+@application.route("/addBestMenuResult", methods=['POST'])
 def reg_bestmenu_submit():
     image_file=request.files["file"]
     image_file.save("static/image/{}".format(image_file.filename))
     data=request.form
     print(list(data.values()))
     
-    if DB.insert_menu(data['menu__name'], data, image_file.filename):
-        return render_template("showBestMenu.html", data=data, image_path="/static/image/"+image_file.filename) 
+    if DB.insert_menu(data['menu_name'], data, image_file.filename):
+        return render_template("addBestMenuResult.html", data=data, img_path="/static/image/"+image_file.filename) 
     else:
         return "menu name already exist!"
 
-@application.route("/addReview")
-def reg_review():
-    return render_template("addReview.html")
+@application.route("/addReviewResult")
+def reg_review_result():
+    return render_template("addReviewResult.html")
 
-@application.route("/showReview")
-def view_review():
-    return render_template("showReview.html")
-
-@application.route("/showReview", methods=['POST']) 
+@application.route("/addReviewResult", methods=['POST'])
 def reg_review_submit():
     image_file=request.files["file"]
     image_file.save("static/image/{}".format(image_file.filename))
     data=request.form
     print(list(data.values()))
     
-    if DB.insert_review(data['review__reviewer'], data, image_file.filename):
-        return render_template("showReview.html", data=data, image_path="/static/image/"+image_file.filename) 
+    if DB.insert_review(data['review_reviewer'], data, image_file.filename):
+        return render_template("addReviewResult.html", data=data, img_path="/static/image/"+image_file.filename) 
+    
+@application.route("/addReview")
+def reg_review():
+    return render_template("addReview.html")
+
+#@application.route("/showReview", methods=['POST']) 
+#def reg_review_submit():
+#    image_file=request.files["file"]
+#    image_file.save("static/image/{}".format(image_file.filename))
+#    data=request.form
+#    print(list(data.values()))
+    
+#    if DB.insert_review(data['review_reviewer'], data, image_file.filename):
+#        return render_template("showReview.html", data=data, image_path="/static/image/"+image_file.filename) 
 
 @application.route("/result", methods=['POST']) 
 def reg_restaurant_submit():
@@ -74,12 +90,71 @@ def reg_restaurant_submit():
     image_file.save("static/image/{}".format(image_file.filename))
     data=request.form
     print(list(data.values()))
+    print(data)
     
     if DB.insert_restaurant(data['restaurant_name'], data, image_file.filename):
         return render_template("result.html", data=data, image_path="/static/image/"+image_file.filename) 
     else:
         return "Restaurant name already exist!"
     
+
+@application.route("/showAllRestaurantList")
+def list_all_restaurants():
+    page = request.args.get("page", 0, type=int)
+    limit = 6
+    
+    start_idx=limit*page
+    end_idx=limit*(page+1)
+    data = DB.get_restaurants() #read the table
+    tot_count = len(data)
+    print(data)
+    data = dict(list(data.items())[start_idx:end_idx])
+    
+    return render_template(
+        "showAllRestaurantList.html",
+        datas=data.items(),
+        total=tot_count,
+        limit=limit,
+        page=page,
+        page_count=int((tot_count/6)+1))
+
+@application.route("/view_detail/<name>/")
+def view_restaurant_detail(name):
+    data = DB.get_restaurant_byname(str(name))
+    avg_rate = DB.get_avgrate_byname(str(name))
+
+    print("####data:",data)
+    return render_template("showRestaurantDetail.html", data=data, avg_rate=avg_rate)
+
+@application.route("/list_foods/<res_name>/")
+def view_foods(res_name):
+    
+    data = DB.get_food_byname(str(res_name))
+    tot_count = len(data)
+    page_count = len(data)
+    
+    return render_template(
+        "showBestMenu.html",
+        datas=data,
+        total=tot_count)
+
+@application.route("/list_reviews/<res_name>/")
+def view_reviews(res_name):
+    res_data = DB.get_restaurant_byname(str(res_name))
+    data = DB.get_review_byname(str(res_name))
+    
+    avg_rate = DB.get_avgrate_byname(str(res_name))
+    tot_count = len(data)
+    page_count = len(data)
+    
+    return render_template(
+        "showReview.html",
+        data=res_data,
+        datas=data,
+        total=tot_count,
+        avg_rate=avg_rate)
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)
+    
     
