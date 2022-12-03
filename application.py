1a860f869470d5ad597505f3416ab2f9508b2ccf
JooheeHwang <2170095@ewhain.net>
2022-12-03 15:07:08 +0000

    백엔드 팀 작업
    - 식당 추천 화면의 이미지들을 각각의 식당 세부 화면과 연결시켰습니다.

--- application.py
+++ application.py
@@ -205,5 +205,43 @@ def add_menus(res_name):
         data=res_data
         )
 
+###############showRecommendationList 동적라우팅###################
+
+@application.route("/showRecommendationList/<hashtag>/")
+def list_hashtag_restaurants():
+    page = request.args.get("page", 0, type=int)
+    hashtag = request.args.get("hashtag", "전체")
+    limit = 6
+    
+    start_idx=limit*page
+    end_idx=limit*(page+1)
+    
+    data = DB.get_restaurants_byhash(str(name))
+    
+ #   data = DB.get_restaurants() #read the table
+    tot_count = len(data)
+    print("hashtag",hashtag,tot_count)
+    if tot_count<=limit:
+        data = dict(list(data.items())[:tot_count])
+    else:
+        data = dict(list(data.items())[start_idx:end_idx])
+    data = dict(sorted(data.items(), key=lambda x: x[1]['name'], reverse=False))
+    print(data)
+    
+    page_count = len(data)
+    print(tot_count,page_count)
+    return render_template(
+        "showRecommendationList.html",
+        datas=data.items(),
+        total=tot_count,
+        limit=limit,
+        page=page,
+        page_count=math.ceil(tot_count/6),
+        hashtag=hashtag)
+
+###############################################################
+
+
 if __name__ == "__main__":
     application.run(host='0.0.0.0', debug=True)     
+    
